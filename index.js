e78af6110e0befec433aaba97a18a1e019fd581e
bhc <behicsakar@gmail.com>
2024-05-20 14:38:25 +0300

    refactor: Remove unused Discord Copilot workflow and index.js file

--- index.js
+++
@@ -1,9 +0,0 @@
-addEventListener("fetch", (event) => {
-  event.respondWith(handleRequest(event.request));
-});
-
-async function handleRequest(request) {
-  return new Response("Hello, World!", {
-    headers: { "content-type": "text/plain" },
-  });
-}
