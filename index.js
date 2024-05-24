37561b2d44e5c6c1c4563f58047334490862c0b7
xue <behicsakar@gmail.com>
2024-05-24 17:22:38 +0300

    refactor: Improve error handling and update response in index.js

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
+export default async function ({ req, res, log, error }) {
+  try {
+    // Log a message
+    log("Function is running");
 
-  // You can log messages to the console
-  log("Hello, Logs!");
-
-  // If something goes wrong, log an error
-  error("Hello, Errors!");
-
-  // The `req` object contains the request data
-  if (req.method === "GET") {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    return res.send("Hello, World!");
+    // Check the request method
+    if (req.method === "GET") {
+      // Send a JSON response
+      return res.json({ message: "Hello, World!" });
+    } else {
+      // Handle other HTTP methods if necessary
+      return res.send("Unsupported request method", 405);
+    }
+  } catch (err) {
+    // Log any errors
+    error(err.message);
+    return res.send("An error occurred", 500);
   }
-
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: "Build like a team of hundreds_",
-    learn: "https://appwrite.io/docs",
-    connect: "https://appwrite.io/discord",
-    getInspired: "https://builtwith.appwrite.io",
-  });
-};
+}
