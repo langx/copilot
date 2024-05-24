ebf444d423d591308060735ff9a780572fe4ce49
xue <behicsakar@gmail.com>
2024-05-24 17:14:31 +0300

    refactor: Remove unnecessary code and improve error handling in index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 export default async function (req, res) {
-  if (!res || typeof res.json !== "function") {
-    throw new TypeError(
-      "Response object is not defined or does not have a json method"
-    );
-  }
-
   res.json({
     message: `Hello, World!`,
   });
