312c6cc6d43ad7c455195b5424c60cffed6a0522
xue <behicsakar@gmail.com>
2024-05-28 22:59:43 +0300

    refactor: Add error handling for missing required fields in index.js

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import { handleInteraction } from "./copilot/aiHandler.js";
+import { throwIfMissing } from "./utils/utils.js";
 
 export default async function ({ req, res, log, error }) {
   try {
     // Log a message
     log("Function is running");
+    log(`req: ${JSON.stringify(req)}`);
 
     // Log some data from the request body
     if (req.body) {
@@ -16,10 +18,18 @@ export default async function ({ req, res, log, error }) {
       log(`res: ${res}`);
       return res.json({ message: "Hello, World!" });
     } else if (req.method === "POST") {
+      throwIfMissing(JSON.parse(req.body), ["message"]);
+      throwIfMissing(req.headers, ["x-appwrite-user-id"]);
       // Handle POST request
       // log(`req.body: ${JSON.parse(req.body)}`);
+      // log(`req.headers: ${req.headers["x-appwrite-user-id"]}`);
+
+      //TODO:  Check user has early-adopter badge or not!
+
       const userMessage = JSON.parse(req.body).message;
       log(`userMessage: ${userMessage}`);
+      log(`userId: ${req.headers["x-appwrite-user-id"]}`);
+
       const aiResponse = await handleInteraction(userMessage);
       return res.json({ response: aiResponse });
     } else {
